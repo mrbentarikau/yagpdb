e9ffa72e59a707689656fb54a72fdb747e9be9f2
caubert <caubert@gmail.com>
2020-07-10 11:42:04 +0300

    context.go kindof

--- common/templates/context.go
+++ common/templates/context.go
@@ -77,10 +77,9 @@ var (
 		"cslice":             CreateSlice,
 		"complexMessage":     CreateMessageSend,
 		"complexMessageEdit": CreateMessageEdit,
-		"kindOf":	      KindOf,
+		"kindOf":             KindOf,
 
 		"formatTime":  tmplFormatTime,
-		"kindOf":      tmplKindOf,
 		"json":        tmplJson,
 		"in":          in,
 		"inFold":      inFold,
@@ -501,7 +500,7 @@ func baseContextFuncs(c *Context) {
 
 	c.ContextFuncs["addRoleID"] = c.tmplAddRoleID
 	c.ContextFuncs["removeRoleID"] = c.tmplRemoveRoleID
-	
+
 	c.ContextFuncs["addRoleName"] = c.tmplAddRoleName
 	c.ContextFuncs["removeRoleName"] = c.tmplRemoveRoleName
 
@@ -590,17 +589,17 @@ func MaybeScheduledDeleteMessage(guildID, channelID, messageID int64, delaySecon
 type Dict map[interface{}]interface{}
 
 func (d Dict) Set(key interface{}, value interface{}) string {
-    d[key] = value
-    return ""
+	d[key] = value
+	return ""
 }
 
 func (d Dict) Get(key interface{}) interface{} {
-    return d[key]
+	return d[key]
 }
 
 func (d Dict) Del(key interface{}) string {
-    delete(d, key)
-    return ""
+	delete(d, key)
+	return ""
 }
 
 type SDict map[string]interface{}
